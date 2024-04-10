3c8cc984af1de93b4e83eb5c7db85dee4aa69ea2
jorboare <jordi.b.arevalo@gmail.com>
2024-04-10 15:38:15 +0200

    Prettier config added. Experience texts modified

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -51,7 +51,7 @@ const Experience = () => {
       object.style.opacity = "1";
       object.style.height = calcHeight + "px";
     } else if (calcHeight > elPosY) {
-      object.style.height = elPosY + 3 + "px";
+      object.style.height = elPosY + 4 + "px";
     }
 
     if (calcHeight > 300) {
@@ -64,14 +64,14 @@ const Experience = () => {
     <>
       <div
         id="experience"
-        className="relative w-full sm:pt-14 sm:mt-[0px] md:mt-[50px] lg:mt-[120px]"
+        className="relative w-full pb-10 sm:mt-[0px] sm:pt-14 md:mt-[50px] lg:mt-[120px]"
       >
-        <div className="fadeInRight absolute sm:w-full md:w-2/3 h-full md:h-[500px] lg:h-[600px] sm:top-0 md:-top-1/2 right-0 z-0 gradient-photo"></div>
-        <div className="sm:col-span-12 md:col-span-8 z-40">
-          <h1 className="fadeInLeft  col-span-12 sm:text-center md:text-center font-accent p-0 text-primary leading-[10rem] sm:text-5xl md:text-[150px]  z-0">
+        <div className="gradient-photo absolute right-0 z-0 h-full fadeInRight sm:top-0 sm:w-full md:-top-1/2 md:h-[500px] md:w-2/3 lg:h-[600px]"></div>
+        <div className="z-40 mb-10 sm:col-span-12 md:col-span-8">
+          <h1 className="fadeInLeft  z-30 col-span-12 p-0 font-accent leading-[10rem] text-primary sm:text-center sm:text-5xl md:text-center md:text-[150px]">
             <span className="text-secondary">E</span>xperience
           </h1>
-          <p className="fadeInLeft z-50 text-primary sm:text-lg md:text-xl lg:text-2xl font-primary max-w-[700px] mr-auto">
+          <p className="fadeInLeft z-50 max-w-[700px] font-primary text-primary sm:text-lg md:mb-10 md:ml-[20%] md:text-xl lg:text-2xl">
             With experience spanning both front-end and back-end development, my
             expertise lies in crafting visually compelling experiences using
             React.
@@ -79,7 +79,7 @@ const Experience = () => {
         </div>
         <div
           id="verticalLine"
-          className="transition-width absolute sm:left-0 md:left-1/2 top-full -translate-x-1/2 -tranlate-y-1/2 w-[1px] bg-primary z-40"
+          className="-tranlate-y-1/2 absolute top-full z-40 mt-10 w-[1px] -translate-x-1/2 bg-primary transition-width sm:left-0 md:left-1/2"
         ></div>
       </div>
       <div>
